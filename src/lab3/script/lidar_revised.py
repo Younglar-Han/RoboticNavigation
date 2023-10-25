fbea9758a6474139500333fc4b07bfa06ed9b487
Younglar-Han <3369883232@qq.com>
2023-10-25 14:04:13 +0800

    feat: add min and max detect

--- src/lab3/script/lidar_revised.py
+++ src/lab3/script/lidar_revised.py
@@ -6,9 +6,12 @@ import sensor_msgs.msg
 
 pub = rospy.Publisher('/revised_scan', LaserScan, queue_size = 10)
 scann = LaserScan()
+real_range_max_golbal = 0.0
+real_range_min_global = 10.0
 
 def callback(msg):
-    #print(len(msg.ranges)) len is 2019 from 0-360
+    global real_range_max_golbal
+    global real_range_min_global
     current_time = rospy.Time.now()
     scann.header.stamp = current_time
     scann.header.frame_id = 'laser'
@@ -16,13 +19,24 @@ def callback(msg):
     scann.angle_max = 6.28318458203
     scann.angle_increment = 0.0174532923847
     scann.time_increment = 2.98899994959e-05
-    scann.scan_time = 0.0
+    scann.scan_time = msg.scan_time
     scann.range_min = 0.119999997318
     scann.range_max = 3.5
-    scann.ranges = msg.ranges[0:72]
-    scann.intensities = msg.intensities[0:72]
-    print(scann)
-    pub.publish(scann)
+    scann.ranges = msg.ranges
+    scann.intensities = msg.intensities
+    real_range_max = max(msg.ranges)
+    
+    real_range_min = 10.0
+    for i in range(0, len(msg.ranges)):
+        if msg.ranges[i] < real_range_min and msg.ranges[i] > 0.01:
+            real_range_min = msg.ranges[i]
+    if real_range_max > real_range_max_golbal:
+        real_range_max_golbal = real_range_max
+    if real_range_min < real_range_min_global:
+        real_range_min_global = real_range_min
+    print('real range max =', real_range_max_golbal, '  real range min =', real_range_min_global)
+    
+    # pub.publish(scann)
 
 def listener():
     rospy.init_node('revised_scan', anonymous=True)
