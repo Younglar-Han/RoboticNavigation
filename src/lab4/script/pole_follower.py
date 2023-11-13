a631bd79c5686436b4de1eb4f3bc553dcfa39b5e
Younglar-Han <3369883232@qq.com>
2023-11-13 20:41:06 +0800

    feat: finish lab4: follower a pole

--- src/lab4/script/pole_follower.py
+++ src/lab4/script/pole_follower.py
@@ -2,16 +2,28 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 
 def callback(msg):
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
     range_min = 10.0
     for i in range(len(msg.ranges)):
         if msg.ranges[i] < range_min and msg.ranges[i] != 0.0:
             range_min = msg.ranges[i]
             range_min_index = i
     print('min:', range_min, 'index: ', range_min_index)
-    # rospy.loginfo(rospy.get_caller_id() + 'I heard')
+
+    twist = Twist()
+    if (range_min < 1.5 and range_min > 0.2):
+        if range_min_index > 10 and range_min_index < 180:
+            twist.angular.z = (range_min_index - 10)/180.0 * 1.7 + 0.5
+        elif range_min_index > 180 and range_min_index < 350:
+            twist.angular.z = (range_min_index - 350)/180.0 * 1.7 - 0.5
+        elif range_min_index > 350 or range_min_index < 10:
+            twist.linear.x = 0.21
+    print(twist.angular.z)
+    pub.publish(twist)
 
 
 def pole_follower():
