a8d2424a8332e47b63a62249ef879364227a674b
Younglar-Han <3369883232@qq.com>
2023-11-15 15:01:54 +0800

    feat: finish the pole follower

--- src/lab4/script/pole_follower.py
+++ src/lab4/script/pole_follower.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-
+import numpy as np
 
 def callback(msg):
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
@@ -15,14 +15,52 @@ def callback(msg):
     print('min:', range_min, 'index: ', range_min_index)
 
     twist = Twist()
+    
+    ## method 1
     if (range_min < 1.5 and range_min > 0.2):
         if range_min_index > 10 and range_min_index < 180:
-            twist.angular.z = (range_min_index - 10)/180.0 * 1.7 + 0.5
+            twist.angular.z = (range_min_index - 10)/180.0 * 2.0 + 0.5
         elif range_min_index > 180 and range_min_index < 350:
-            twist.angular.z = (range_min_index - 350)/180.0 * 1.7 - 0.5
+            twist.angular.z = (range_min_index - 350)/180.0 * 2.0 - 0.5
         elif range_min_index > 350 or range_min_index < 10:
             twist.linear.x = 0.21
-    print(twist.angular.z)
+    
+    # method 2 (worse than method 1)
+    # if (range_min < 1.5 and range_min > 0.2):
+    #     if range_min_index > 0 and range_min_index < 180:
+    #         twist.angular.z = range_min_index/180.0 * 2.0 + 0.5
+    #         twist.linear.x = range_min / 1.5 * 0.11 + 0.1
+    #     elif range_min_index > 180 and range_min_index < 360:
+    #         twist.angular.z = (range_min_index - 360)/180.0 * 2.0 - 0.5
+    #         twist.linear.x = range_min / 1.5 * 0.11 + 0.1
+    
+    ## method 3
+    # if (range_min < 1.5 and range_min > 0.2):
+    #     x = -range_min * np.cos(range_min_index/180.0 * 3.14)  # x = -r*cos(theta) (in the world frame)
+    #     y = -range_min * np.sin(range_min_index/180.0 * 3.14)  # y = -r*sin(theta) (in the world frame)
+    #     k_ro = 0.1
+    #     k_alpha = 0.4
+    #     k_beta = 0.0
+    #     alpha = range_min_index/180.0 * 3.14
+    #     beta = -np.arctan2(y, x)
+    #     rho = np.sqrt(x**2 + y**2)
+    #     v = k_ro * rho
+    #     w = k_alpha * alpha + k_beta * beta
+    #     print('x:', x, 'y:', y, 'alpha:', alpha, 'beta:', beta, 'rho:', rho)
+    #     print('v:', v, 'w:', w)
+    #     if w > 2.5:
+    #         w = 2.5
+    #     elif w < -2.5:
+    #         w = -2.5
+    #     if v > 0.21:
+    #         v = 0.21
+    #     elif v < -0.21:
+    #         v = -0.21
+    #     twist.linear.x = v
+    #     twist.angular.z = w
+
+        
+    print('linear.x:', twist.linear.x, 'angular.z:', twist.angular.z)
     pub.publish(twist)
 
 
